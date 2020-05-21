4f0953d9e59474362786f70c93c6b7667bface02
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2020-05-21 00:04:25 -0400

    changes

--- App.py
+++ App.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = '0.0.0.0' #'remotemysql.com'
+app.config['MYSQL_USER'] = 'root' #'oNcACxFstL'
+app.config['MYSQL_PASSWORD'] = 'root' #'Hct8mjydD2'
+app.config['MYSQL_DB'] = 'testapp' #'oNcACxFstL'
+# app.config['MYSQL_PORT'] = 3306
+mysql = MySQL(app)
+
 @app.route('/')
 def Index():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/addContact', methods=["POST"])
+def addContact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        # 'cur' allow us to exec mysql queries
+        cur = mysql.connect.cursor()
+        cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)', 
+        (fullname, phone, email))
+        mysql.connection.commit()
+
+        return jsonify({"msg": "received"})
+
+@app.route('/edit')
+def editContact():
+    return jsonify({"msg": 'editContact'})
+
+@app.route('/delete')
+def deleteContact():
+    return jsonify({"msg": 'deleteContact'})
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
-
---
+++ Readme.md
@@ -0,0 +1,15 @@
+https://phoenixnap.com/kb/mysql-docker-container
+
+https://medium.com/better-programming/setting-up-mysql-database-in-a-docker-d6c69a3e9afe
+
+docker run --detach --name=mysql --env="MYSQL_ROOT_PASSWORD=root" --publish 3306:3306 mysql
+
+
+
+error:
+
+You should try pip3 install --upgrade setuptools.
+sudo apt-get install python3.6-dev libmysqlclient-dev
+
+
+
